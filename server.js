6841898637629fba89f872c2c60d9721252ba46d
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-03-28 13:43:13 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
-// Trust the first proxy
-app.set("trust proxy", 1);
 
 app.use(express.json());
 app.use(cors());
